04feb8c350d7d3c6f76c6e4f554acd7eb2fc3169
Cableo204 <oliveradamcable@gmail.com>
2017-11-09 18:15:41 +0000

    Recovery: Replacing roocli since disk ran out of space...
    (-_-)

--- roocli.py
+++ roocli.py
@@ -6,32 +6,32 @@ from subprocess import call
 
 @click.group()
 def cli():
+    pass
+
+@cli.command()
+@click.option('--name', '-n', default='', help='Provide a human friendly, project name for your wordpress container, defaults to the commit SHA.')
+@click.option('--commit', '-c', default='master', help='Provide a Git commit Hash to pull a specific Wordpress version.')
+@click.option('--port', '-p', default='8080', help='Provide a port to access sonarqube over.')
+
+def build(name, commit, port):
     """
     Used to deploy a containerized wordpress app.\n
     Example:\n
       - To build: `roo-wp --name sherbertLemon --hash 5etr638u --port 24601`
       - To destroy: `roo-wp --name sherbertLemon` 
     """
-    pass
-
-@cli.command()
-@click.option('--name', '-n', default='', help='Provide a human friendly, project name for your wordpress container, defaults to the commit SHA.')
-@click.option('--hash', '-h', default='master', help='Provide a Git commit Hash to pull a specific Wordpress version.')
-@click.option('--port', '-p', default='8080', help='Provide a port to access sonarqube over.')
-
-def build(name, hash, port):
     # If user didn't provide a name, use the hash for tagging.
     if not name:
-        name = hash
+        name = commit
 
     # Attempt to run Wordpress.
-    run_image(name, port)
+    run_image(name, commit, port)
 
-def run_image(name, port):
+def run_image(name, commit, port):
     # Prepare docker-compose command for call module.
     build_cmd = ''
-    build_cmd += 'NAME=' + name + ' '
-    build_cmd += 'HASH=' + hash + ' '
+    build_cmd += 'IMAGE=roo/wordpress:' + name + ' '
+    build_cmd += 'COMMIT=' + commit + ' '
     build_cmd += 'PORT=' + port + ' '
     build_cmd += 'docker-compose '
     build_cmd += '--project-name ' + name + ' '
