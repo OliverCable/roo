3fd70a9ade03e80b4da2a1022d004109b5b655c4
Cableo204 <oliveradamcable@gmail.com>
2017-11-09 18:45:11 +0000

    Added simple remove feature, necessitates user knows the name/commit to destory though

--- roocli.py
+++ roocli.py
@@ -30,16 +30,37 @@ def build(name, commit, port):
 def run_image(name, commit, port):
     # Prepare docker-compose command for call module.
     build_cmd = ''
-    build_cmd += 'IMAGE=roo/wordpress:' + name + ' '
-    build_cmd += 'COMMIT=' + commit + ' '
+    build_cmd += 'NAME=' + name + ' '
+    build_cmd += 'HASH=' + commit + ' '
     build_cmd += 'PORT=' + port + ' '
     build_cmd += 'docker-compose '
     build_cmd += '--project-name ' + name + ' '
-    build_cmd += 'up -d -force-recreate'
+    build_cmd += 'up -d'
     click.echo(build_cmd)
 
+    # Execute build command.
     try:
         call([build_cmd],shell=True)
+    except:
+        click.echo("Docker compose has failed: please check your inputs, " +
+                   "or manually try the build to debug: `" + str(build_cmd) + "`")
+
+@cli.command()
+@click.option('--name', '-n', default='', help='Provide a human friendly, project name for your wordpress container, defaults to the commit SHA.')
+@click.option('--commit', '-c', default='master', help='Provide a Git commit Hash to pull a specific Wordpress version.')
+
+def destroy(name, commit):
+    # If user didn't provide a name, use the hash for tagging.
+    if not name:
+        name = commit
+
+    # Prepare destroy command.
+    destroy_cmd = 'PORT=80 docker-compose '
+    destroy_cmd += '-p ' + name + ' down'
+
+    # Execute destroy command.
+    try:
+        call([destroy_cmd],shell=True)
     except:
         click.echo("Docker compose has failed: please check your inputs, " +
                    "or manually try the build to debug: `" + str(build_cmd) + "`")
